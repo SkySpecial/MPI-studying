feacbbfd3b91bf4cbf6954845fe2327a28951d94
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-18 00:50:19 +0300

    Update and rename hello.c to how_many_processes.c

--- how_many_processes.c
+++ how_many_processes.c
@@ -4,12 +4,19 @@
 int main(int argc, char ** argv)
 {
 	int proc_num;
+	int proc_rank;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size( MPI_COMM_WORLD, &proc_num);
+	MPI_Comm_rank( MPI_COMM_WORLD, &proc_rank);
+	
+        if( 0 == proc_rank)
+       		 printf("%d\n", proc_num);
 
 	MPI_Finalize();
 
-	printf("%d\n", proc_num);
+	//if( 0 == proc_rank)
+	//	printf("%d\n", proc_num);
+
 	return 0;
 }	
