7f1e0df56b84e19d3715c57675db86af06610fd7
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-10-27 21:54:33 +0300

    Это - двоичный компаратор.
    В чём ошибка? Каждый раз выдаёт "Second number is bigger!"

---
+++ MPI_Bin_comparator.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <mpi.h>
+
+#define MAX_NUM 1000 // запускать на 10 процессах
+
+int dec_to_bin( int );
+
+int main(int argc, char **argv)
+{
+	int num1, num2, proc_num, proc_rank, comp_result, i = 0;
+	int buf1[10], buf2[10], buf_result[10]; 
+	MPI_Status status;
+
+	MPI_Init( &argc, &argv );
+	MPI_Comm_size( MPI_COMM_WORLD, &proc_num );
+	MPI_Comm_rank( MPI_COMM_WORLD, &proc_rank );
+
+	if( 0 == proc_rank ) // считываем десятичные числа в 0 процессе, они известны только ему
+	{
+		scanf("%d%d", &num1, &num2 );
+
+        	if( num1>1000 ) // ограничения 
+                	num1 = MAX_NUM;
+
+        	if( num2>1000 )
+                	num2 = MAX_NUM;
+
+		MPI_Send( &num1, 1, MPI_INT, 1, 0, MPI_COMM_WORLD ); // сделать другой коммуникатор - разослать только им?
+		MPI_Send( &num2, 1, MPI_INT, 2, 0, MPI_COMM_WORLD ); // посылаем числа процессу 1 и 2
+	}
+	
+	if( 1 == proc_rank ) // перевод в бинарный вид в этих процессах
+	{
+		MPI_Recv( &num1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status );
+		buf1[i] = dec_to_bin( num1 );
+	}
+	
+	if( 2 == proc_rank)
+	{
+		MPI_Recv( &num2, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status );
+		buf2[i] = dec_to_bin( num2 );
+	}
+
+	MPI_Bcast( buf1, 10, MPI_INT, 1, MPI_COMM_WORLD ); // теперь бинарный вид у всех
+	MPI_Bcast( buf2, 10, MPI_INT, 2, MPI_COMM_WORLD );
+
+	comp_result = ( buf1[proc_rank] == buf2[proc_rank] ) ? 0 : 1; 	// каждый процесс пар-но сравнивает разряд
+
+	MPI_Barrier( MPI_COMM_WORLD );
+	
+	MPI_Gather( &comp_result, 1, MPI_INT, buf_result, 10, MPI_INT, 0, MPI_COMM_WORLD );	// рез-т сравнения на 0 процессе
+	
+	if ( 0 == proc_rank )
+	{
+		i = 0;
+		while( 0 == buf_result[i] ) // По порядку анализируем разряды чисел - равны или не равны
+			i++; 
+		
+		if( buf1[i] > buf2[i] )
+			printf("\nFirst number is bigger!\n");
+		else printf("\nSecond number is bigger!\n");
+	}
+
+	MPI_Finalize();
+	return 0;
+}
+
+int dec_to_bin( int num)
+{
+	int i = 0, buf[10];
+
+        while( num != 1 )
+        { 
+        	buf[i] = num%2;
+                num /= 2;
+                i++;
+        }
+
+        buf[i] = 1;
+        i++;
+
+       	while( i <= 9)
+        {
+        	buf[i] = 0;
+                i++;
+        }
+
+        for( i=0; i<10; i++ )
+       		buf[i] = buf[9-i];
+
+	return buf[i];
+}	
