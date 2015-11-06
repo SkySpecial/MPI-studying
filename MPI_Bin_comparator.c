a0dda576351eaf58b5bb934e8a4047e9ccf252d1
SkySpecial <dmitrii.cherepanov@phystech.edu>
2015-11-06 22:32:48 +0300

    Update MPI_Bin_comparator.c

--- MPI_Bin_comparator.c
+++ MPI_Bin_comparator.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
 #define MAX_NUM 1000 // запускать на 10 процессах
 
-int dec_to_bin( int );
+void dec_to_bin( int );
+
+int buf[10]; // глобальный массив надо занулять? Или он тогда неправильно работать будет?
 
 int main(int argc, char **argv)
 {
-	int num1, num2, proc_num, proc_rank, comp_result, i = 0;
+	int num1, num2, proc_num, proc_rank, comp_result, i;
 	int buf1[10], buf2[10], buf_result[10]; 
 	MPI_Status status;
 
@@ -15,6 +18,14 @@ int main(int argc, char **argv)
 	MPI_Comm_size( MPI_COMM_WORLD, &proc_num );
 	MPI_Comm_rank( MPI_COMM_WORLD, &proc_rank );
 
+	if( 10 != proc_num ) // проверка на 10 процессов
+	{
+		if( 0 == proc_rank ) printf("Wrong number of processes!\n");
+                MPI_Finalize();
+		return 0;
+        }
+
+	
 	if( 0 == proc_rank ) // считываем десятичные числа в 0 процессе, они известны только ему
 	{
 		scanf("%d%d", &num1, &num2 );
@@ -32,13 +43,31 @@ int main(int argc, char **argv)
 	if( 1 == proc_rank ) // перевод в бинарный вид в этих процессах
 	{
 		MPI_Recv( &num1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status );
-		buf1[i] = dec_to_bin( num1 );
+		dec_to_bin( num1 );
+
+		printf("The first number is: ");
+		
+		for( i=0;i<10;i++)
+		{
+			buf1[i] = buf[i];
+			printf("%d ", buf1[i]);
+		}
+		printf("\n");
 	}
 	
 	if( 2 == proc_rank)
 	{
 		MPI_Recv( &num2, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status );
-		buf2[i] = dec_to_bin( num2 );
+		dec_to_bin( num2 );
+
+		printf("The second number is: ");
+
+		for(i=0;i<10;i++)
+		{
+			buf2[i] = buf[i];
+			printf("%d ", buf2[i] );
+		}
+		printf("\n");
 	}
 
 	MPI_Bcast( buf1, 10, MPI_INT, 1, MPI_COMM_WORLD ); // теперь бинарный вид у всех
@@ -48,26 +77,41 @@ int main(int argc, char **argv)
 
 	MPI_Barrier( MPI_COMM_WORLD );
 	
-	MPI_Gather( &comp_result, 1, MPI_INT, buf_result, 10, MPI_INT, 0, MPI_COMM_WORLD );	// рез-т сравнения на 0 процессе
+	MPI_Gather( &comp_result, 1, MPI_INT, buf_result, 1, MPI_INT, 0, MPI_COMM_WORLD );	// рез-т сравнения на 0 процессе
 	
 	if ( 0 == proc_rank )
 	{
+		for(i=0;i<10;i++)
+			printf("%d ", buf_result[i] ); 
 		i = 0;
+		int flag = 1;
+
 		while( 0 == buf_result[i] ) // По порядку анализируем разряды чисел - равны или не равны
-			i++; 
+		{
+			i++;
+			
+			if( 10 == i)
+			{
+				printf("Numbers are equal!\n");
+				flag = 0;
+			}
+		} 
 		
-		if( buf1[i] > buf2[i] )
-			printf("\nFirst number is bigger!\n");
-		else printf("\nSecond number is bigger!\n");
+		if(flag)
+		{
+			if( buf1[i] > buf2[i] )
+				printf("\nFirst number is bigger!\n");
+			else printf("\nSecond number is bigger!\n");
+		}
 	}
 
 	MPI_Finalize();
 	return 0;
 }
 
-int dec_to_bin( int num)
+void dec_to_bin( int num)
 {
-	int i = 0, buf[10];
+	int i = 0;
 
         while( num != 1 )
         { 
@@ -87,6 +131,4 @@ int dec_to_bin( int num)
 
         for( i=0; i<10; i++ )
        		buf[i] = buf[9-i];
-
-	return buf[i];
 }	
